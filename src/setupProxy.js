9c5a3592e8750de870d957f2131b9068f5fcb3b5
soreilly@syncstats.com <soreilly@syncstats.com>
2022-06-16 13:29:11 -0400

    ch

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
-    createProxyMiddleware({
+    createProxyMiddleware(
+    '/',
+    {
       target: 'http://localhost:1337',
       changeOrigin: true,
     })
